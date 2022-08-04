281d347da1256027493e3eb59c28d375a40671e6
zq654 <103563700+zq654@users.noreply.github.com>
2022-08-04 20:40:50 +0800

    Add files via upload

--- LeetCode_2.java
+++ LeetCode_2.java
@@ -1,39 +1,52 @@
-//第一版思路把链表转成整数然后再相加但是会爆int
 public class LeetCode_2 {
    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      int a1=CountNumber(l1);
-      int a2=CountNumber(l2);
-      int count=a1+a2;
-      int act=count%10;
-      ListNode head=new ListNode(act);
-      count=count/10;
-      ListNode pointer=head;
-      while(count!=0){
-         int value=count%10;
-         count=count/10;
-         ListNode Next=new ListNode(value);
-         pointer.next=Next;
-         pointer=pointer.next;
-      }
-      pointer.next=null;
-      return head;
+     l1=Reversal(l1);//反转链表
+     l2=Reversal(l2);
+      ListNode l=new ListNode();
+      BulidList(l1,l2,l,0);
+      return Reversal(l.next);
    }
 
-   static int CountNumber(ListNode l) {
-      int num=0;
-      int res=0;
-      while(l!=null){
-         res+=(int)(Math.pow(10,num)*l.val);
-         l=l.next;
+   private void BulidList(ListNode l1, ListNode l2,ListNode l, int i) {
+      if(l1==null&&l2==null){
+         if(i!=0)l.next=new ListNode(1,null);
+         return;
+      }else{
+         if(l1==null){
+            l.next=new ListNode((i+l2.val)%10,null);
+            BulidList(l1,l2.next,l.next,(i+ l2.val)/10);
+         }
+         if(l2==null){
+            l.next=new ListNode((i+l1.val)%10,null);
+            BulidList(l1.next,l2,l.next,(l1.val+ i)/10);
+         }
+         if(l1!=null&&l2!=null){
+            l.next=new ListNode((l1.val+l2.val+i)%10,null);
+            BulidList(l1.next,l2.next,l.next,(l1.val+l2.val+i)/10);
+         }
       }
-      return res;
+   }
+
+   private static ListNode Reversal(ListNode l) {
+      ListNode head=new ListNode(0,null);
+      ListNode pointer=l.next;
+      while(l.next!=null){
+         l.next=head.next;
+         head.next=l;
+         l=pointer;
+         pointer=pointer.next;
+      }
+      l.next=head.next;
+      head.next=l;
+      return head.next;
    }
 
    public static void main(String[] args) {
-      ListNode l1=new ListNode(5);
-
-      ListNode l2=new ListNode(2);
-      System.out.println(CountNumber(l1));
+      ListNode a=new ListNode(9,null);
+      ListNode b=new ListNode(4,a);
+      ListNode c=new ListNode(2,b);
+      ListNode d=Reversal(c);
+      System.out.println(d.val);
    }
 }
 
